fc7ab2790e57b2b1155a04e5eeb23f1e2723eda6
se-fit <bhusan@bhusans-MacBook-Pro.local>
2018-04-26 13:22:54 +0545

    init commit

---
+++ src/client/client.js
@@ -0,0 +1,6 @@
+// starting point for the client side application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      This is the Home component
+        <button onClick={() => console.log('clicked')}>Click</button>
+    </div>
+  );
+}
+
+export default Home;
